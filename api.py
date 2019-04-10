88461bd883737628bf6033da09a43624a12b1390
Alessandro Santos Ferreira <alessandroferreira@softbox.network>
2019-04-10 11:06:14 -0300

    ajuste url api

--- api.py
+++ api.py
@@ -2,9 +2,10 @@
 
 import json
 import time
-from bottle import route, request,run, response
+from bottle import route, request, run, response
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+
 
 @route('/info', method='POST')
 def buscaInfo():
@@ -25,15 +26,20 @@ def buscaInfo():
             driver.find_element_by_id("passwordNext").click()
             time.sleep(5)
             driver.get("https://drive.google.com/u/0/settings/storage?hl=pt-BR")
-            info_utilizado = driver.find_element_by_css_selector('.H01aEe').get_attribute('innerHTML')
-            info_total = driver.find_element_by_css_selector('.xl7bTb').get_attribute('innerHTML')
-            ret = {"email": email,"utilizado": info_utilizado,"total": info_total}
+            info_utilizado = driver.find_element_by_css_selector(
+                '.H01aEe').get_attribute('innerHTML')
+            info_total = driver.find_element_by_css_selector(
+                '.xl7bTb').get_attribute('innerHTML')
+            ret = {"email": email, "utilizado": info_utilizado,
+                   "total": info_total}
             response.content_type = 'application/json'
             return json.dumps(ret, sort_keys=True)
 
         except Exception as inst:
             print(inst)
-            return {"error": True, "msg":"erro ao buscar informacoes do e-mail: " + email}
+            return {"error": True, "msg": "erro ao buscar informacoes do e-mail: " + email}
     else:
-        return {"error":True, "msg":"e-mail nao informado"}
+        return {"error": True, "msg": "e-mail nao informado"}
+
+
 run(host='localhost', port=8090, debug=True)
